9a9b2266621a77e863529a34903a0f88eca9a237
vviswadeep <44389755+vviswadeep@users.noreply.github.com>
2024-01-05 17:23:55 +0530

    Display in UI

--- src/app/device/device.component.ts
+++ src/app/device/device.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { AngularDeviceInformationService } from 'angular-device-information';
 
@@ -9,21 +9,32 @@ import { AngularDeviceInformationService } from 'angular-device-information';
 })
 export class DeviceComponent implements OnInit {
   constructor(
-    private deviceInformationService: AngularDeviceInformationService
+    public deviceInformationService: AngularDeviceInformationService
   ) {
+    this.isMobile = deviceInformationService.isMobile();
     console.log(deviceInformationService.isMobile()); // returns if the device is a mobile device (android / iPhone / windows-phone etc)
+    this.isTablet = deviceInformationService.isTablet();
     console.log(deviceInformationService.isTablet()); // returns if the device is a tablet (tablet iPad etc)
+    this.isDesktop = deviceInformationService.isDesktop();
     console.log(deviceInformationService.isDesktop()); // returns if the app is running on a Desktop browser.
+    this.getDeviceType = deviceInformationService.getDeviceType();
     console.log(deviceInformationService.getDeviceType()); // returns if the app is running on a Desktop browser.
-    console.log(deviceInformationService.getDeviceInfo().os); // returns os name like Windows/Andtoid/iOS/Linux/Mac OS X etc
-    console.log(deviceInformationService.getDeviceInfo().osVersion); // returns os version like 10/8.1/7 ...etc
-    console.log(deviceInformationService.getDeviceInfo().browser); // returns browser name like chrome/firefox ...etc
-    console.log(deviceInformationService.getDeviceInfo().browserVersion); // returns browser version as number
-    console.log(deviceInformationService.getDeviceInfo().browserMajorVersion); // returns full browser version as number
-    console.log(deviceInformationService.getDeviceInfo().screen_resolution); // returns screnn size like 1390x860/640x800 ...etc
-    console.log(deviceInformationService.getDeviceInfo().cookies); // returns cookies enabled or no
-    console.log(deviceInformationService.getDeviceInfo().userAgent); // returns userAgent
+    this.getDeviceInfo = deviceInformationService.getDeviceInfo();
+    console.log(deviceInformationService.getDeviceInfo());
+    // console.log(deviceInformationService.getDeviceInfo().os); // returns os name like Windows/Andtoid/iOS/Linux/Mac OS X etc
+    // console.log(deviceInformationService.getDeviceInfo().osVersion); // returns os version like 10/8.1/7 ...etc
+    // console.log(deviceInformationService.getDeviceInfo().browser); // returns browser name like chrome/firefox ...etc
+    // console.log(deviceInformationService.getDeviceInfo().browserVersion); // returns browser version as number
+    // console.log(deviceInformationService.getDeviceInfo().browserMajorVersion); // returns full browser version as number
+    // console.log(deviceInformationService.getDeviceInfo().screen_resolution); // returns screnn size like 1390x860/640x800 ...etc
+    // console.log(deviceInformationService.getDeviceInfo().cookies); // returns cookies enabled or no
+    // console.log(deviceInformationService.getDeviceInfo().userAgent); // returns userAgent
   }
 
+  @Input() isMobile: boolean;
+  @Input() isTablet: boolean;
+  @Input() isDesktop: boolean;
+  @Input() getDeviceType: string;
+  @Input() getDeviceInfo: any;
   ngOnInit() {}
 }
